54b916ae76eb5e2593622bff272f176dbd2918eb
= <=>
2018-12-20 15:12:54 +0300

    Register background image changed, poster.jpg image added into images folder

--- VidlyPrototype/Migrations/201812081007146_PopulateRentalsTable.cs
+++ VidlyPrototype/Migrations/201812081007146_PopulateRentalsTable.cs
@@ -7,10 +7,10 @@ namespace VidlyPrototype.Migrations
     {
         public override void Up()
         {
-            Sql("INSERT INTO Rentals(CustomerId, MovieId, DateRented,DateReturned) VALUES(1, 1, '"+DateTime.Now+"','10 dec 2018')");
-            Sql("INSERT INTO Rentals(CustomerId, MovieId, DateRented,DateReturned) VALUES(3, 2, '"+DateTime.Now+"','12 dec 2018')");
-            Sql("INSERT INTO Rentals(CustomerId, MovieId, DateRented,DateReturned) VALUES(3, 6, '"+DateTime.Now+"','15 dec 2018')");
-            Sql("INSERT INTO Rentals(CustomerId, MovieId, DateRented,DateReturned) VALUES(9, 9, '"+DateTime.Now+"','16 dec 2018')");
+            //Sql("INSERT INTO Rentals(CustomerId, MovieId, DateRented,DateReturned) VALUES(1, 1, '"+DateTime.Now+"','10 dec 2018')");
+            //Sql("INSERT INTO Rentals(CustomerId, MovieId, DateRented,DateReturned) VALUES(2, 2, '"+DateTime.Now+"','12 dec 2018')");
+            //Sql("INSERT INTO Rentals(CustomerId, MovieId, DateRented,DateReturned) VALUES(2, 6, '"+DateTime.Now+"','15 dec 2018')");
+            //Sql("INSERT INTO Rentals(CustomerId, MovieId, DateRented,DateReturned) VALUES(3, 9, '"+DateTime.Now+"','16 dec 2018')");
 
 
         }
